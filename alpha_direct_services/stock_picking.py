d17a935243774775ae210a3ad2a32193bc85a01a
Max Mumford <mmu@openerp.com>
2013-10-24 19:47:41 +0200

    [FIX] wrong super class

--- alpha_direct_services/stock_picking.py
+++ alpha_direct_services/stock_picking.py
@@ -13,19 +13,19 @@ def all_assigned(picking_obj, cr, ids):
 
 class stock_picking(osv.osv):
     """ Inherit the stock.picking object to and add ads_sent and ads_result fields """
-    
+
     _inherit = 'stock.picking'
     _columns = {
         'ads_sent': fields.boolean('Sent to ADS?', help="This field indicates whether or not this document has been uploaded to Alpha Direct Systems for processing."),
         'ads_result': fields.text('ADS Send Results', help="If there are any errors while sending this document to ADS, they will be logged here"),
     }
-    
+
     def action_process(self, cr, uid, ids, context=None):
         if all_assigned(self, cr, ids):
             raise osv.except_osv(_('Cannot Process Manually'), _("The picking should be processed in the ADS system. It will then be automatically synchronized to OpenERP."))
         else:
             super(stock_picking, self).action_process(cr, uid, ids, context=context)
-    
+
     def action_cancel(self, cr, uid, ids, context=None):
         if all_assigned(self, cr, ids):
             raise osv.except_osv(_('Cannot Cancel'), _("You can't cancel a picking when it is in 'Ready to Receive' state because it has already been sent to ADS.") )
@@ -40,18 +40,18 @@ class stock_picking_in(osv.osv):
         'ads_sent': fields.boolean('Sent to ADS?', help="This field indicates whether or not this document has been uploaded to Alpha Direct Systems for processing."),
         'ads_result': fields.text('ADS Send Results', help="If there are any errors while sending this document to ADS, they will be logged here"),
     }
-    
+
     def action_process(self, cr, uid, ids, context=None):
         if all_assigned(self, cr, ids):
             raise osv.except_osv(_('Cannot Receive Manually'), _("The picking should be received in the ADS system. It will then be automatically synchronized to OpenERP."))
         else:
-            super(stock_picking, self).action_process(cr, uid, ids, context=context) 
-    
+            super(stock_picking_in, self).action_process(cr, uid, ids, context=context)
+
     def action_cancel(self, cr, uid, ids, context=None):
         if all_assigned(self, cr, ids):
             raise osv.except_osv(_('Cannot Cancel'), _("You can't cancel a picking when it is in 'Ready to Receive' state because it has already been sent to ADS.") )
         else:
-            super(stock_picking, self).action_cancel(cr, uid, ids, context=context)
+            super(stock_picking_in, self).action_cancel(cr, uid, ids, context=context)
 
 class stock_picking_out(osv.osv):
     """ Inherit the stock.picking.in object to and add ads_sent and ads_result fields """
@@ -61,15 +61,15 @@ class stock_picking_out(osv.osv):
         'ads_sent': fields.boolean('Sent to ADS?', help="This field indicates whether or not this document has been uploaded to Alpha Direct Systems for processing."),
         'ads_result': fields.text('ADS Send Results', help="If there are any errors while sending this document to ADS, they will be logged here"),
     }
-    
+
     def action_process(self, cr, uid, ids, context=None):
         if all_assigned(self, cr, ids):
             raise osv.except_osv(_('Cannot Deliver Manually'), _("The picking should be marked as delivered in the ADS system. It will then be automatically synchronized to OpenERP."))
         else:
-            super(stock_picking, self).action_process(cr, uid, ids, context=context) 
-    
+            super(stock_picking_out, self).action_process(cr, uid, ids, context=context)
+
     def action_cancel(self, cr, uid, ids, context=None):
         if all_assigned(self, cr, ids):
             raise osv.except_osv(_('Cannot Cancel'), _("You can't cancel a picking when it is in 'Ready to Receive' state because it has already been sent to ADS.") )
         else:
-            super(stock_picking, self).action_cancel(cr, uid, ids, context=context)
+            super(stock_picking_out, self).action_cancel(cr, uid, ids, context=context)
