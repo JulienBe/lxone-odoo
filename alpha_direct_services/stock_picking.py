874f0adf412ccc2e38b6374778d96f001d66ba11
Max Mumford <mmu@openerp.com>
2013-11-26 18:21:16 +0100

    [ADD] hide in cancel button [ADD] move in process button [ADD] add in re-upload button

--- alpha_direct_services/stock_picking.py
+++ alpha_direct_services/stock_picking.py
@@ -1,6 +1,7 @@
 from openerp.tools.translate import _
 from openerp.osv import osv, fields
 from ads_sales_order import ads_sales_order
+from ads_purchase_order import ads_purchase_order
 
 def all_assigned(picking_obj, cr, ids):
     for picking in picking_obj.read(cr, 1, ids, ['state']):
@@ -41,11 +42,19 @@ class stock_picking_in(osv.osv):
     def action_disallow_invoicing(self, cr, uid, ids, context=None):
         self.write(cr, uid, ids, {'invoice_state': 'none'})
 
-    def action_process(self, cr, uid, ids, context=None):
-        if all_assigned(self, cr, ids):
-            raise osv.except_osv(_('Cannot Receive Manually'), _("The picking should be received in the ADS system. It will then be automatically synchronized to OpenERP."))
-        else:
-            super(stock_picking_in, self).action_process(cr, uid, ids, context=context)
+    def ads_manuel_upload(self, cr, uid, ids, context=None):
+        """ Upload this picking to ADS """
+        for picking_id in ids:
+            picking = self.browse(cr, uid, picking_id, context=context)
+
+            # make sure state is correct
+            if not picking.state == 'assigned':
+                continue
+
+            data = ads_purchase_order(picking)
+            data.upload(cr, self.pool.get('ads.manager'))
+        return True
+
 
 class stock_picking_out(osv.osv):
     """ Inherit the stock.picking.in to prevent manual processing and cancellation after ads upload """
