1be1ea3b9bf2424ec0b32f412eafd76837aa8d60
Max Mumford <mmu@openerp.com>
2014-02-28 15:11:00 +0100

    [ADD] unfinished execute functions

--- lx-one/lx_update.py
+++ lx-one/lx_update.py
@@ -13,25 +13,45 @@ class lx_update(osv.osv):
     _name = 'lx.update'
 
     _columns = {
-        'sequence': fields.char('Execution Sequence'),
+        'sequence': fields.char('Execution Sequence', required=True, readonly=True),
         'state': fields.selection( (
                 ('to_execute', 'To Execute'), 
                 ('executed', 'Executed'), 
                 ('failed', 'Failed')
             ), 'State'),
-        'object_type': fields.char('Object Type', size=12),
-        'data': fields.text('Data'),
-        'result': fields.text('Execution Result'),
-        'file_name': fields.char('File Name', size=64),
-        'node_number': fields.integer('XML Node Number'),
+        'object_type': fields.char('Object Type', size=12, required=True, readonly=True),
+        'data': fields.text('Data', required=True,),
+        'result': fields.text('Execution Result', readonly=True),
+        'file_name': fields.char('File Name', size=64, required=True, readonly=True),
+        'node_number': fields.integer('XML Node Number', required=True, readonly=True),
     }
 
     _defaults = { 
+        'state': 'to_execute',
+        'result': '',
         'sequence': lambda obj, cr, uid, context: obj.pool.get('ir.sequence').get(cr, uid, 'lx.update')
     }
 
     def execute(self, cr, uid, ids, context=None):
-        raise NotImplementedError("This method should execute this update's data")
+        """
+        Sorts IDs by their sequence, process them, then set state to executed
+        """
+        updates = self.read(cr, uid, ids, ['sequence'], context=context)
+        updates.sort(key=lambda update: int(update['sequence']))
+        for update in updates:
+            update = self.browse(cr, uid, update['id'], context=context)
+            
+            # do importation
+            try:
+                result = 'Picking closed'
+            
+                # change state
+                update.write({'state': 'executed', 'result': result})
+            except Exception, e:
+                result = 'Error while executing: %s' % str(e)
+                update.write({'state': 'failed', 'result': result})
 
-    def execute_all(self, cr, uid, context=None):
-        raise NotImplementedError("This method should search for all to_execute and failed updates and execute them")
+    def execute_all(self, cr, uid, ids=[], context=None):
+        """ Gets ids for all updates whose state is not imported and calls execute on them """
+        all_ids = self.search(cr, uid, [('state', '!=', 'imported')], context=context)
+        self.execute(cr, uid, all_ids)
